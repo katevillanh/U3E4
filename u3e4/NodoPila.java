abb42a13644714712227b2b0fa3b6cf77f2798e8
Kate Villanueva <71800218+katevillanh@users.noreply.github.com>
2020-11-28 12:15:43 -0600

    Create NodoPila.java

---
+++ u3e4/NodoPila.java
@@ -0,0 +1,14 @@
+
+package u3e4;
+
+public class NodoPila {
+   int elemento;
+    NodoPila siguiente;
+    NodoPila siguiente2;
+    public NodoPila(int elemento){
+        this.elemento=elemento;
+        this.siguiente=null;
+        this.siguiente2=null;
+    }
+    
+}
